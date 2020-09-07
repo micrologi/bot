4611d8f2fd323e1011350c05e4a095c64a566e10
Marlon Andrei <marlon@micrologi.com.br>
2020-09-07 16:40:51 -0300

    Adição de comandos que instala packages necessários e adição do bat

---
+++ cmd/botstart.bat
@@ -0,0 +1,9 @@
+npm install -g npm
+
+npm install restify
+npm install --save botbuilder-dialogs
+npm install --save botbuilder-ai
+npm install dotenv
+
+npm install -g yo generator-botbuilder
+yo botbuilder
